892f9149d2b4c69eec34cd469aab7f11ea96ca01
Jhoan Villa <jhoaned@users.noreply.github.com>
2018-01-08 12:41:36 -0500

    Update facturas_model.php
    Comentarios

--- models/facturas_model.php
+++ models/facturas_model.php
@@ -1,14 +1,19 @@
 <?php
+    /**
+    * Clase factura
+    * Gestiona el acceso de los datos referentes a las facturas
+    **/
     class facturas_model{
-        private $db;
-        private $detalle;
-        private $productosf;
+        private $db; //conexión a la base de datos
+        private $productosf;  //Listado de productos de la factura
 
         public function __construct(){
+            //Creación de conexión
             $this->db=Conectar::conexion();
             $this->facturas=array();
         }
         public function get_productosfactura_id($id){
+            //Listado de productos de una factura específica
             $consulta=$this->db->query("SELECT df.producto_detalle as 'idfd', p.nombre_producto as 'productofd', df.cantidad as 'cantidadfd', p.valor_unitario as 'valorunifd', df.valor_total as 'valortfd' FROM detalle_factura df, producto p, factura f WHERE f.id_factura=df.factura_detalle AND f.id_factura=".$id." AND df.producto_detalle=p.id_producto;");
             while($filas=$consulta->fetch_assoc()){
                 $this->productosf[]=$filas;
@@ -16,10 +21,12 @@
             return $this->productosf;
         }
         public function set_detalle($nuevo_detalle){
+            //Inserción de productos a una factura
             $this->resultado=$this->db->query("insert into detalle_factura (factura_detalle, producto_detalle, cantidad, valor_total) values (".$nuevo_detalle[0].",".$nuevo_detalle[1].",".$nuevo_detalle[2].",".$nuevo_detalle[3].");");
             return $this->resultado;
         }
         public function set_factura($nueva_factura){
+            //Inserción de una nueva factura
             $this->resultado=$this->db->query("insert into factura (fecha_factura, cliente_factura, total_factura) values ('".$nueva_factura[0]."',".$nueva_factura[1].",".$nueva_factura[2].");");
             return $this->resultado;
         }
