42a7ae72c8f31d4f39c2edbe72619f9dbeb475b5
Jhoan Villa <jhoaned@users.noreply.github.com>
2018-01-08 12:58:29 -0500

    Update factura_cliente_controller.php
    Comentarios

--- controllers/factura_cliente_controller.php
+++ controllers/factura_cliente_controller.php
@@ -1,4 +1,8 @@
 <?php
+	/**
+	*Controlador de detalles de facturas
+	*Carga del listado de productos de un factura
+	**/
 	include("../models/facturas_model.php");
 	include("../db/db.php");
 	$id=$_GET['facid'];
