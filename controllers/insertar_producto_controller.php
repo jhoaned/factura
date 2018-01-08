dfd13e18558446b96de9af3305206cfe1c3400ce
Jhoan Villa <jhoaned@users.noreply.github.com>
2018-01-08 13:02:17 -0500

    Update insertar_producto_controller.php
    Comentarios

--- controllers/insertar_producto_controller.php
+++ controllers/insertar_producto_controller.php
@@ -1,4 +1,8 @@
 <?php
+	/**
+	*Controlador de inserción de productos
+	*Obtiene datos de la vista y los almacena por medio del modelo respectivo
+	**/
 	include("../models/productos_model.php");
 	include("../db/db.php");
 	
