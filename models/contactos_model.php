7786cc47f8c3ceceb4ab7a47230133395e9f4e29
Jhoan Villa <jhoaned@users.noreply.github.com>
2018-01-08 12:46:05 -0500

    Update contactos_model.php
    Comentarios

--- models/contactos_model.php
+++ models/contactos_model.php
@@ -1,13 +1,19 @@
 <?php
+    /**
+    * Clase contactos
+    * Gestiona el acceso de los tipos de contactos
+    **/
     class contactos_model{
-        private $db;
-        private $contactos;
+        private $db; //conexión a la base de datos
+        private $contactos; //Listado de contactos
 
         public function __construct(){
+            //Creación de conexión
             $this->db=Conectar::conexion();
             $this->contactos=array();
         }
-        public function get_contactos(){
+        public function get_contactos
+            //Listado de los tipos de contactos
             $consulta=$this->db->query("select * from tipo_contacto;");
             while($filas=$consulta->fetch_assoc()){
                 $this->contactos[]=$filas;
