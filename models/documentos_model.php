a3d3859eb4a3a92533efb85ae4f4840ed3255c9c
Jhoan Villa <jhoaned@users.noreply.github.com>
2018-01-08 12:43:19 -0500

    Update documentos_model.php
    Comentarios

--- models/documentos_model.php
+++ models/documentos_model.php
@@ -1,13 +1,19 @@
 <?php
+    /**
+    * Clase documentos
+    * Gestiona el acceso de los tipos de documentos
+    **/
     class documentos_model{
-        private $db;
-        private $documentos;
+        private $db; //conexión a la base de datos
+        private $documentos; //Listado de documentos
 
         public function __construct(){
+            //Creación de conexión
             $this->db=Conectar::conexion();
             $this->documentos=array();
         }
         public function get_documentos(){
+            //Listado de los tipos de documentos
             $consulta=$this->db->query("select * from tipo_documento;");
             while($filas=$consulta->fetch_assoc()){
                 $this->documentos[]=$filas;
